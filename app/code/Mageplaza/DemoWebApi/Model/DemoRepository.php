df890215e874ea66ed3f38e04ec4b6a9911c748e
YVic95 <vika.yevlentieva@gmail.com>
2022-09-30 18:01:39 +0300

    implementation of save and getById methods added

--- app/code/Mageplaza/DemoWebApi/Model/DemoRepository.php
+++ app/code/Mageplaza/DemoWebApi/Model/DemoRepository.php
@@ -7,6 +7,9 @@ use Mageplaza\DemoWebApi\Api\Data\DemoInterfaceFactory;
 use Mageplaza\DemoWebApi\Model\DemoResource;
 use Mageplaza\DemoWebApi\Model\DemoCollectionFactory;
 use Mageplaza\DemoWebApi\Api\Data\DemoSearchResultsInterfaceFactory;
+use Mageplaza\DemoWebApi\Api\Data\DemoInterface;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface;
 
 class DemoRepository implements DemoRepositoryInterface
@@ -30,5 +33,39 @@ class DemoRepository implements DemoRepositoryInterface
         $this->demoCollectionFactory = $demoCollectionFactory;
         $this->searchResultsFactory = $searchResultsFactory;
         $this->collectionProcessor = $collectionProcessor;
+    }
+
+    /**
+     * Save box.
+     *
+     * @param \Mageplaza\DemoWebApi\Api\Data\DemoInterface $box
+     * @return \Mageplaza\DemoWebApi\Api\Data\DemoInterface
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function save(DemoInterface $box)
+    {
+        try {
+            $this->demoResourceModel->save($box);
+        } catch(\Exception $e) {
+            throw new CouldNotSaveException(__($e->getMessage()));
+        }
+        return $box;
+    }
+
+    /**
+     * Retrieve items box.
+     *
+     * @param int $boxId
+     * @return \Mageplaza\DemoWebApi\Api\Data\DemoInterface
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getById($boxId)
+    {
+        $box = $this->demoFactory->create();
+        $this->demoResourceModel->load($box, $boxId);
+        if(!$box->getId()) {
+            throw new NoSuchEntityException(__('Box with id "%1" does not exist'));
+        }
+        return $box;
     }
 }
