67f129b6ba1e6a93ec10acaf7fadc3278790f376
YVic95 <vika.yevlentieva@gmail.com>
2022-11-10 19:22:05 +0200

    ressuply class for resupply listing column

--- app/code/Mageplaza/MyInventory/Ui/Component/Listing/Columns/Resupply.php
+++ app/code/Mageplaza/MyInventory/Ui/Component/Listing/Columns/Resupply.php
@@ -3,18 +3,43 @@
 namespace Mageplaza\MyInventory\Ui\Component\Listing\Columns;
 
 use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Framework\UrlInterface;
 
 class Resupply extends Column
 {
     protected $urlBuilder;
 
-    public function __construct()
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    )
     {
-
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
     }
 
     public function prepareDataSource(array $dataSource)
     {
+        if(isset($dataSource['data']['items'])) {
+            $storeId = $this->context->getFilterParam('store_id');
+
+            foreach($dataSource['data']['items'] as &$item) {
+                $item[$this->getData('name')]['resupply'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                            'myinventory/product/resupply', 
+                            ['id' => $item['entity_id'], 'store' => $storeId]
+                    ),
+                    'label' => __('Resupply'),
+                    'hidden' => false
+                ];
+            }
+        }
+
         return $dataSource;
     }
 }
