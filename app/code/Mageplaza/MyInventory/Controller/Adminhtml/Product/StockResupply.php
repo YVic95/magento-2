7a2b7f2bd96c9d807e54014b73b392b638a651e4
YVic95 <vika.yevlentieva@gmail.com>
2022-11-14 14:13:03 +0200

    form cintroller

---
+++ app/code/Mageplaza/MyInventory/Controller/Adminhtml/Product/StockResupply.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Mageplaza\MyInventory\Controller\Adminhtml\Product;
+
+use Mageplaza\MyInventory\Controller\Adminhtml\Product;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Backend\App\Action\Context;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\CatalogInventory\Api\StockRegistryInterface;
+use Mageplaza\MyInventory\Model\Resupply;
+
+class StockResupply extends Product
+{
+    protected $stockRegistry;
+    protected $productRepository;
+    protected $resupply;
+
+    public function __construct(
+        Context $context,
+        ProductRepositoryInterface $productRepository,
+        StockRegistryInterface $stockRegistry,
+        Resupply $resupply
+    )
+    {
+        parent::__construct($context);
+        $this->stockRegistry = $stockRegistry;
+        $this->productRepository = $productRepository;
+        $this->resupply = $productRepository;
+    }
+
+    public function execute()
+    {
+        if($this->getRequest()->isPost()) {
+            $this->resupply->resupply(
+                $this->getRequest()->getParam('id'),
+                $_POST['myinventory_product']['qty']
+            );
+            $this->messageManager->addSuccessMessage(__('Successfully resuplied'));
+            $redirectResult = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            return $redirectResult->setPath('myinventory/product/index');
+        } else {
+            $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+            $resultPage->getConfig()->getTitle()->prepend((__('Stock Resupply')));
+            return $resultPage;
+        }
+    }
+}
